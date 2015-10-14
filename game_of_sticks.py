004e702986c976c107beb652233a890386f41bc6
Some Guy <abc@abc.com>
2015-10-13 22:24:56 -0700

    Getting closer with Ai

--- game_of_sticks.py
+++ game_of_sticks.py
@@ -19,22 +19,27 @@ class Game:
     def play_game(self, player1, player2):
         self.running = True
         self.num_sticks = self.choose_num_sticks()
+        #current_player = player1
         while self.running:
             print("{} it is your turn.".format(player1))
             self.show_sticks()
             self.remove_sticks(player1.choose_num(self.num_sticks))
             if self.is_sticks_empty():
-                self.end_game(player2)
-                break
-            print("{} it is your turn.".format(player2))
-            self.show_sticks()
-            self.remove_sticks(player2.choose_num(self.num_sticks))
-            if self.is_sticks_empty():
-                self.end_game(player1)
+                self.end_game(player2, player1)
+            else:
+                print("{} it is your turn.".format(player2))
+                self.show_sticks()
+                self.remove_sticks(player2.choose_num(self.num_sticks))
+                if self.is_sticks_empty():
+                    self.end_game(player1, player2)
 
-    def end_game(self, player):
+    def end_game(self, winner, loser):
         self.running = False
-        print("Game over! {} wins".format(player))
+        print("Game over! {} wins and {} loses".format(winner, loser))
+        if isinstance(winner, Ai):
+            winner.append_win()
+        if isinstance(loser, Ai):
+            loser.append_loss()
 
     def is_sticks_empty(self):
         if self.num_sticks <= 0:
@@ -84,8 +89,86 @@ class Player:
                 print("Please enter only a number")
 
 
+class Ai(Player):
+
+    def __init__(self, y):
+        super().__init__(y)
+        self.hats = {num: [1, 2, 3] for num in range(1, 101)}
+        self.dump = {}
+
+    def __str__(self):
+        return "Robot"
+
+    def choose_num(self, num_sticks):
+        num_choice = 0
+        while num_choice == 0:
+            try:
+                if num_sticks == 1:
+                    choice = int(input("Enter 1 to pick up the last stick! > "))
+                    if choice == 1:
+                        self.dump[num_sticks] = choice
+                        return choice
+                    else:
+                        print("One stick is your only option loser!")
+                elif num_sticks == 2:
+                    choice = int(input("How many sticks do you want to pick up? (1-2) > "))
+                    if 1 <= choice <= 2:
+                        self.dump[num_sticks] = choice
+                        return choice
+                    else:
+                        print("Only 1 or 2 sticks please.")
+                else:
+                    choice = int(input("How many sticks do you want to pick up? (1-3) > "))
+                    if 1 <= choice <= 3:
+                        self.dump[num_sticks] = choice
+                        return choice
+                    else:
+                        print("Only a number 1 - 3")
+            except ValueError:
+                print("Please enter only a number")
+
+    def append_win(self):
+        for hat, values in self.hats.items():
+            for thing, choice in self.dump.items():
+                if hat == thing:
+                    values.append(choice)
+        self.dump = {}
+
+    def append_loss(self):
+        for hat, values in self.hats.items():
+            for thing, choice in self.dump.items():
+                if hat == thing:
+                    if values.count(choice) > 1:
+                        values.remove(choice)
+                        values.sort()
+        self.dump = {}
+
 if __name__ == '__main__':
     Peter = Player('Pete')
     Adam = Player('Adam')
-    new_game = Game()
-    new_game.play_game(Peter, Adam)
+    #new_game = Game()
+    #new_game.play_game(Peter, Adam)
+    Robot = Ai("Mr. Robot")
+    game2 = Game()
+    game2.play_game(Peter, Robot)
+    print(Robot.dump)
+    print(Robot.hats)
+    game3 = Game()
+    game3.play_game(Peter, Robot)
+    print(Robot.dump)
+    print(Robot.hats)
+    game4 = Game()
+    game5 = Game()
+    game4.play_game(Peter, Robot)
+    print(Robot.dump)
+    print(Robot.hats)
+    game5.play_game(Peter, Robot)
+    print(Robot.dump)
+    print(Robot.hats)
+
+
+    #print(Robot.hats)
+    #print(type(Robot))
+    #if isinstance(Robot, Ai):
+        #print("YES YES YES ")
+    #print(Robot)
