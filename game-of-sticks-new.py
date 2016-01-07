22eb850564707192da0c3fea0c661692b19f3fd8
jp <jp.park@me.com>
2016-01-07 15:37:09 -0800

    final version

--- game-of-sticks-new.py
+++ game-of-sticks-new.py
@@ -2,87 +2,82 @@ print("Welcome to the Game of Sticks!")
 
 
 class Game:
+    def __init__(self, player1, player2):
+        self.user_choice = input("How many sticks are there on the table initially (10-100)? \n")
+        self.player1 = player1
+        self.player2 = player2
+        self.current_player = self.player1
+        self.number_of_sticks = 100
+        self.winner = True
+        self.choice = 0
 
-    def __init__(self):
-        self.game_play = False
-        self.game_sticks = 100
-
-    def game_sticks_chosen(self):
-        user_num = 0
-        while user_num == 0:
-            try:
-                user_num = int(input("How many sticks are there on the table initially (10-100)?"))
-                if 10 <= user_num <= 100:
-                    return user_num
-                else:
-                    print("Please enter a number between 10 and 100")
-            except ValueError:
-                print("Told you to choose between 10 -100 sticks")
-
-    def game_start(self, player_1, player_2):
-        self.game_running = True
-        self.game_sticks = self.game_sticks_chosen()
-        while self.game_running:
-            print("{}'s turn".format(player_1))
-            # self.game_sticks_picked()
-            self.game_sticks_removed(player_1.game_sticks_chosen(self.game_sticks))
-            if self.game_sticks_empty():
-                self.game_end(player_2)
-                self.play_again()
-                break
-            print("{}'s turn".format(player_2))
-            # self.game_sticks_picked()
-            self.game_sticks_removed(player_2.game_sticks_chosen(self.game_sticks))
-            if self.game_sticks_empty():
-                self.game_end(player_1)
-                self.play_again()
-
-    def game_end(self, player):
-        self.game_running = False
-        print("{} wins".format(player))
-
-    def play_again(self):
-        while True:
-            again = input("play again? yes or no?")
-            if again == "yes":
-                self.game_start(player_1, player_2)
-            else:
-                print("Thank you for playing")
-            break
-
-    def game_sticks_empty(self):
-        if self.game_sticks <= 0:
-            return True
-
-    def game_sticks_removed(self):
-        if self.game_sticks == 1:
-            print("{} stick(s) remaining".format(self.game_sticks))
+    def start(self):
+        self.get_sticks()
+        while self.number_of_sticks > 1:
+            self.get_choice()
+            self.number_of_sticks -= self.choice
+            if self.number_of_sticks != 1:
+                print("There are {} sticks left on the board".format(self.number_of_sticks))
+            self.switch_players()
+        if self.number_of_sticks <= 0:
+            print("{} wins".format(self.current_player))
         else:
-            print("{} stick(s) remaining".format(self.game_sticks))
+            self.get_winner()
+            print("There is one stick left on the board, {} wins".format(self.winner))
+
+    def get_choice(self):
+        self.user_choice = input("{}, how many sticks would you like to pick up (1 - 3)? \n"
+                                 .format(self.current_player))
+        try:
+            int(self.user_choice)
+        except ValueError:
+            print("Not a valid choice")
+            self.get_choice()
+        self.user_choice = int(self.user_choice)
+        if 1 <= int(self.user_choice) <= 3:
+            self.choice = self.user_choice
+        else:
+            print("Not a valid choice")
+            self.get_choice()
+
+    def get_sticks(self):
+        try:
+            int(self.user_choice)
+            self.number_of_sticks = int(self.user_choice)
+            if 10 <= int(self.user_choice) <= 100:
+                return self.user_choice
+        except ValueError:
+            print("Not a valid choice")
+            self.get_sticks()
+
+    def get_winner(self):
+        if self.current_player == self.player1:
+            self.winner = self.player2
+        else:
+            self.winner = self.player1
+
+    def switch_players(self):
+        if self.current_player == self.player1:
+            self.current_player = self.player2
+        else:
+            self.current_player = self.player1
 
 
 class Player:
-
-    def __init__(self, name):
+    def __init__(self, name="Player__"):
         self.name = name
 
-    def __str__(self, name):
+    def __repr__(self):
         return self.name
 
-    def game_play(self):
-        user_num = 0
-        while user_num == 0:
-            num = int(input("How many sticks do you take (1-3)? "))
-            if 1 <= num <= 3:
-                return num
-            else:
-                print("Please enter a number between 1 and 3")
+    def __str__(self):
+        return self.name
 
 
-
-if __name__== "__main__":
-    player_1 = "player_1"
-    player_2 = "player_2"
-    game_new = Game()
-    game_new.game_play(player_1, player_2)
-
+if __name__ == '__main__':
+    name1 = input("Player 1, What is your name? \n")
+    name2 = input("Player 2, What is your name? \n")
+    p1 = Player(name1)
+    p2 = Player(name2)
+    game = Game(p1, p2)
+    game.start()
